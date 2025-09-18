26da808486f41f56ecd68d959b764a9635c7fa13
lostannu <icyanupam.gmail.com>
2025-09-18 14:31:36 +0530

    testing functionalities

--- src/main/java/com/project/sih25008/Sih25008Application.java
+++ src/main/java/com/project/sih25008/Sih25008Application.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Sih25008Application {
     public static void main(String[] args) {
-
         SpringApplication.run(Sih25008Application.class, args);
     }
 }
